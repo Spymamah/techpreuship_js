db02e55b94a6abb7410206b6497b73edf13cbad5
Your NameSpymamah <ikechukwumamah1@gmail.com>
2025-03-01 17:41:48 +0100

    new

---
+++ generateMultiplication.js
@@ -0,0 +1,9 @@
+function generateMultiplicationTable(n){
+    
+    for(let x = 1; x<=10; x++){
+        
+        console.log(`${n} * ${x} = ${n * x}`)
+    }
+        return n;
+}
+(generateMultiplicationTable(9))
