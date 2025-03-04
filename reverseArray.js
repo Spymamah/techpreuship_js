95884c0200bd42980abda6064120ba3c772e021f
Your NameSpymamah <ikechukwumamah1@gmail.com>
2025-03-04 16:23:45 +0100

    another

---
+++ reverseArray.js
@@ -0,0 +1,12 @@
+function reverseArray(arr){
+    
+    let value = []
+    for(let i = arr.length-1;  i >= 0; i--){
+        value.push(arr[i])
+        
+console.log(value)
+    }
+   
+}
+let me = [1,2,3,4,5,6,7]
+reverseArray(me)
