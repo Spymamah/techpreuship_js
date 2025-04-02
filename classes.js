b6fee23c951f941fb40742a4393881511ea9afe3
Your NameSpymamah <ikechukwumamah1@gmail.com>
2025-04-02 12:47:20 +0100

    all

---
+++ classes.js
@@ -0,0 +1,30 @@
+class automobile{
+    constructor(make, model, year, engine){
+        this.make = make
+        this.model = model
+        this.year = year
+        this.engine = engine
+    }
+}
+
+//Anything you want to do the moment you create the class should be put into the constructor block
+// The # keyword makes it private, e.g: this.#engine = 'V8'
+let myCar = new automobile ("Mercedes", "Jeep", 2018, "V12")
+
+//inheritance is used for a class to inherit attributes of a different class
+//The super keyword uses the properties of the inherited class
+
+class car extends automobile{
+    numberOfWheels;
+    numberOfDoors;
+
+    constructor(wheels, doors, make, model, year, engine){
+        super(make, model, year, engine)
+        this.numberOfDoors = doors;
+        this.numberOfWheels = wheels;
+    }
+}
+
+let Jeep = new car (4, 4, "Mercedes", "Jeep", 2018, "V12")
+
+console.log("My Jeep is a", Jeep)
